e4a410a7c388df0d66a4d0625ca0177676a12283
Miclaine Emtman <clainie@gmail.com>
2024-03-18 12:26:05 -0700

    Add img and quote to home page

--- src/about.js
+++ src/about.js
@@ -4,17 +4,23 @@ export function loadAboutPage() {
     const content = document.querySelector('#content');
 
     const topImgContainer = document.createElement('div');
+    topImgContainer.id = "about-top-img-container";
     const aboutPageTitle = document.createElement('span');
-    aboutPageTitle.textContent = "The Adaptive Skateboard Project";
+    aboutPageTitle.id = "about-title";
+    aboutPageTitle.textContent = "THE ADAPTIVE SKATEBOARD PROJECT";
     const mainImg = document.createElement('img');
     mainImg.src = aboutPageImg;
+    mainImg.id = "about-main-img";
     topImgContainer.appendChild(aboutPageTitle);
     topImgContainer.appendChild(mainImg);
 
     const quoteContainer = document.createElement('div');
+    quoteContainer.id = 'about-quote-container';
     const quote = document.createElement('p');
-    quote.textContent = "I credit it with saving my life post injury…I think I would have been in a far darker place had I not found a way to get back to the sports I love!";
+    quote.id = 'about-quote'
+    quote.textContent = '"I CREDIT IT WITH SAVING MY LIFE POST INJURY…I THINK I WOULD HAVE BEEN IN A FAR DARKER PLACE HAD I NOT FOUND A WAY BACK TO THE SPORTS I LOVE."';
     const realStoriesBtn = document.createElement('button');
+    realStoriesBtn.id = 'about-experience-btn';
     realStoriesBtn.textContent = 'READ REAL STORIES';
     quoteContainer.appendChild(quote);
     quoteContainer.appendChild(realStoriesBtn);
