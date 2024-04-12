c69137224e4839cff4f01732ba54c5d738acbcea
Miclaine Emtman <clainie@gmail.com>
2024-04-12 12:43:24 -0700

    Make resources page mobile friendly

--- src/resources.js
+++ src/resources.js
@@ -26,7 +26,7 @@ export function loadResourcesPage() {
     content.appendChild(programsContainer);
     programsContainer.appendChild(programsTitle);
     programsContainer.appendChild(programsList);
-    const programs = [['Sheckler Foundation', 'https://www.shecklerfoundation.org/'], ['Poseiden Foundation', 'https://www.poseidenfoundation.org/'], ['Life Rolls On', 'https://liferollson.org/'], ['Keep Pushing Inc.', 'https://www.keeppushinginc.com/about'], ['Challenged Athletes Foundation', 'https://www.challengedathletes.org/'], ['Adaptive Action Sports', 'https://adacs.org/'], ['Brazillian Association of Paraskateboard', 'https://www.abpsk.com.br/'], ['Bridge to Skate', 'https://www.bridgetoskate.org/'], ['Grind for Life Series', 'https://www.grindforlife.org/']];
+    const programs = [['Sheckler Foundation', 'https://www.shecklerfoundation.org/'], ['Poseiden Foundation', 'https://www.poseidenfoundation.org/'], ['Life Rolls On', 'https://liferollson.org/'], ['Keep Pushing Inc.', 'https://www.keeppushinginc.com/about'], ['Challenged Athletes Foundation', 'https://www.challengedathletes.org/'], ['Adaptive Action Sports', 'https://adacs.org/'], ['Brazilian Association of Paraskateboard', 'https://www.abpsk.com.br/'], ['Bridge to Skate', 'https://www.bridgetoskate.org/'], ['Grind for Life Series', 'https://www.grindforlife.org/']];
     programs.forEach(function (element) {
         let listElement = document.createElement('li');
         let linkElement = document.createElement('a');
